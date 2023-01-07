0434593178d6eaa59fefb685caf30879b71055cf
Aparajit Garg <aparajit.garg06@gmail.com>
2023-01-08 02:42:55 +0530

    thumbnail video listing done, video playing function started

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,7 +1,9 @@
 var menuIcon = document.querySelector(".menu-icon");
 var sideBar = document.querySelector(".sidebar");
+var largeContainer = document.querySelector(".container");
 
 
 menuIcon.onclick = () => {
     sideBar.classList.toggle("small-sidebar");
+    largeContainer.classList.toggle("large-container");
 }
